5db81130214419b11f072234d710ca9e8bdd6d0a
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-07-24 15:33:26 +0700

    add modal componetn

---
+++ src/components/modal-create-playlist/index.jsx
@@ -0,0 +1,33 @@
+import style from "./style.module.css";
+import { createPortal } from "react-dom";
+import { useEffect } from "react";
+const ModalPlaylist = ({ isOpen, onClose }) => {
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (e.target && e.target.id === "modal") {
+        return onClose();
+      }
+    };
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+      window.addEventListener("click", handleClick);
+    }
+    return () => window.removeEventListener("click", handleClick);
+  }, [isOpen]);
+  return (
+    isOpen &&
+    createPortal(
+      <div id="modal" className={style.modal}>
+        <div id="overlay" className={style.overlay} />
+        <div className={style.modalContent}>
+          <div className={style.header}>test</div>
+          <div className={style.body}>test</div>
+          <div className={style.footer}>test</div>
+        </div>
+      </div>,
+      document.body
+    )
+  );
+};
+
+export default ModalPlaylist;
--- src/components/playlist/index.jsx
+++ src/components/playlist/index.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { FaPlusCircle } from "react-icons/fa";
 import { usePlaylist } from "../../lib/usePlaylist";
 import Button from "../button";
+import ModalPlaylist from "../modal-create-playlist";
 import MusicCard from "../music-card";
 const Playlist = ({ data }) => {
   const { checkSelected, handleSelect, createPlaylist } = usePlaylist();
+  const [isModalOpen, setModalOpen] = useState(true);
   return (
     <div className="playlistContainer">
       {data.map((music) => (
@@ -14,6 +17,12 @@ const Playlist = ({ data }) => {
           isSelected={checkSelected(music.uri)}
         />
       ))}
+      <ModalPlaylist
+        isOpen={isModalOpen}
+        onClose={() => {
+          setModalOpen(false);
+        }}
+      />
       <div style={{ position: "fixed", bottom: 18, right: 18 }}>
         <Button leftIcon={<FaPlusCircle />} onClick={createPlaylist}>
           Create playlist
