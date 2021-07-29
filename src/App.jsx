aa76b425776fc6f7762fbb811e24733a8fa02c66
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-07-29 13:17:40 +0700

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import Index from "./pages/index";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <Provider store={store}>
-      <Index />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Index />
+          </Route>
+        </Switch>
+      </Router>
       <Toaster position="bottom-center" />
     </Provider>
   );
