cc9d7772771884deb0c9adc65b037d4dff08e71b
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-07-29 15:12:00 +0700

    move login token to index component

--- src/pages/create-playlist.jsx
+++ src/pages/create-playlist.jsx
@@ -1,28 +1,7 @@
-import { useEffect } from "react";
 import Tracklist from "../components/tracklist";
 import Main from "../layout/main";
-import { getProfile } from "../lib/spotify";
-import { useUser } from "../lib/useUser";
-import { useDispatch } from "react-redux";
-import { login, storeUserData } from "../store/user";
 
 const Index = () => {
-  const { isAuthenticated, callback } = useUser();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    //check if user not authenticated then read access_token from hash url
-    if (!isAuthenticated && window.location.hash) {
-      const { access_token } = callback();
-      if (access_token) {
-        dispatch(login(access_token));
-        //if access_token is valid, try to get userData using spotify api
-        getProfile(access_token).then((res) => {
-          dispatch(storeUserData(res));
-        });
-      }
-    }
-  }, [dispatch, callback, isAuthenticated]);
   return (
     <>
       <Main>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,26 @@
 import Main from "../layout/main";
+import { login, storeUserData } from "../store/user";
+import { getProfile } from "../lib/spotify";
+import { useUser } from "../lib/useUser";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const Home = () => {
+  const { isAuthenticated, callback } = useUser();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    //check if user not authenticated then read access_token from hash url
+    if (!isAuthenticated && window.location.hash) {
+      const { access_token } = callback();
+      if (access_token) {
+        dispatch(login(access_token));
+        //if access_token is valid, try to get userData using spotify api
+        getProfile(access_token).then((res) => {
+          dispatch(storeUserData(res));
+        });
+      }
+    }
+  }, [dispatch, callback, isAuthenticated]);
   return <Main>Index</Main>;
 };
 
