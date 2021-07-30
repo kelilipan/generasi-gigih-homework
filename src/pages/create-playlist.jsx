487e2850bdab24742143d6ac4f90503f6f19d06b
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-07-30 19:21:18 +0700

    remove fragment

--- src/pages/create-playlist.jsx
+++ src/pages/create-playlist.jsx
@@ -3,12 +3,10 @@ import Main from "../layout/main";
 
 const Index = () => {
   return (
-    <>
-      <Main>
-        <h1 style={{ fontSize: "44px" }}>Select tracks</h1>
-        <Tracklist />
-      </Main>
-    </>
+    <Main>
+      <h1 style={{ fontSize: "44px" }}>Select tracks</h1>
+      <Tracklist />
+    </Main>
   );
 };
 
