5ae16934f70b043f7bb0117a29a20b42ca389fe3
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-08-15 17:58:19 +0700

    feat: my attemp to pre cache fonts

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -23,8 +23,7 @@ clientsClaim();
 // This variable must be present somewhere in your service worker file,
 // even if you decide not to use precaching. See https://cra.link/PWA
 precacheAndRoute(self.__WB_MANIFEST);
-precacheAndRoute(["/fonts/var-inter-latin.woff2"]);//pre cache fonts
-
+precacheAndRoute([{ url: "/fonts/var-inter-latin.woff2" }]); //pre cache fonts
 
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
