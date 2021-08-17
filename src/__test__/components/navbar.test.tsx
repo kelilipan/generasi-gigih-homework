b80be6dc21ffe78fcbd2f64b8661a15a4b90a3e4
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-08-17 16:40:18 +0700

    chore: add test for navbar

---
+++ src/__test__/components/navbar.test.tsx
@@ -0,0 +1,25 @@
+import Navbar from "components/navbar";
+import { render, screen } from "test-utils";
+import loggedIn from "__test__/pages/dummy/logged-in";
+
+describe("navbar component", () => {
+  it("should have login button", () => {
+    render(<Navbar />);
+    const loginButtons = screen.getByText(/Login With Spotify/i);
+    expect(loginButtons).toBeVisible();
+  });
+  //if logged in
+  it("should have search component", () => {
+    render(<Navbar />, { preloadedState: loggedIn });
+    const searchButton = screen.getByRole("button", { name: /search song/i });
+    expect(searchButton).toBeVisible();
+    const searchInput = screen.getByPlaceholderText("Input song name");
+    expect(searchInput).toBeVisible();
+  });
+
+  it("should have profile", () => {
+    render(<Navbar />, { preloadedState: loggedIn });
+    const profile = screen.getByRole("img", { name: "Wisesa" });
+    expect(profile).toBeVisible();
+  });
+});
