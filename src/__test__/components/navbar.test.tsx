62fb99cac9b3c41a6025af82811f136e9e40407d
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-08-20 19:18:43 +0700

    chore: mock API user profile

--- src/__test__/components/navbar.test.tsx
+++ src/__test__/components/navbar.test.tsx
@@ -1,6 +1,20 @@
 import Navbar from "components/navbar";
 import { render, screen } from "test/utils";
 import user from "test/dummy/user";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { SPOTIFY_API_BASE_URL } from "site.config";
+import { getProfile } from "lib/spotify";
+
+const server = setupServer(
+  rest.get(`${SPOTIFY_API_BASE_URL}/me`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(user.data));
+  })
+);
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
 
 describe("navbar component", () => {
   it("should have login button", () => {
@@ -17,9 +31,24 @@ describe("navbar component", () => {
     expect(searchInput).toBeVisible();
   });
 
-  it("should have profile", () => {
-    render(<Navbar />, { preloadedState: { user } });
-    const profile = screen.getByRole("img", { name: user.data.display_name });
-    expect(profile).toBeVisible();
+  it("should have profile", async () => {
+    const token = "a random token string";
+    const userData = await getProfile(token);
+    render(<Navbar />, {
+      preloadedState: {
+        user: {
+          isAuthenticated: true,
+          token: token,
+          data: userData,
+        },
+      },
+    });
+    const profilePicture = screen.getByRole("img", {
+      name: userData.display_name as string,
+    });
+    const displayName = screen.getByText(userData.display_name as string);
+
+    expect(profilePicture).toBeVisible();
+    expect(displayName).toBeVisible();
   });
 });
